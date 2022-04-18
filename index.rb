87e3f61e0aa43ddff132c05bffd0038490cc1a50
VittorioBianco <vicbianco83@gmail.com>
2022-04-18 14:57:30 +1000

    removed redundant lines. Created class

--- index.rb
+++ index.rb
@@ -1,15 +1,9 @@
-# class FivebyFive
-#   attr_reader(:chest, :back)
-# end
 descr = File.read("./5x5_workout.txt") 
 week1 = File.read("./Week_1.txt")
 week2 = File.read("./Week_2.txt")
-# descr = "placeholder1" 
-# week1 = "placeholder2" 
-# week2 = "placeholder3" 
 require 'colorize'
 while true
-  puts "Welcome to the 5x5 Workout terminal application! Please select from 1-5".colorize(:blue)
+  "Welcome to the 5x5 Workout terminal application! Please select from 1-5".colorize(:light_blue).each_char {|c| putc c ; sleep 0.05}
   puts "\n1) WHAT IS 5X5\n2) WEEK 1 EXERCISES\n3) WEEK 2 EXERCISES\n4) LOG A WORKOUT\n5) EXIT".colorize(:blue)
   input = $stdin.gets.chomp.to_i
     case input
@@ -28,6 +22,13 @@ while true
       puts "Goodbye!".colorize(:red)
       exit
     else
-    puts "Please select a valid input."
+    puts "#{input} is not valid. Please select 1-5 and press enter!".colorize(:red)
     end
+end
+
+class FivebyFive
+attr_accessor :workedout, :log
+@workedout = false
+@log = []
+
 end
