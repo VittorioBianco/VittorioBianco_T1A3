65e279edf8699bf05283153ad77646d4cd8741da
VittorioBianco <vicbianco83@gmail.com>
2022-04-22 10:14:14 +1000

    added ascii banner

--- index.rb
+++ index.rb
@@ -4,6 +4,7 @@ descr = File.read('./5x5_workout.txt')
 week1 = File.read('./Week_1.txt')
 week2 = File.read('./Week_2.txt')
 require 'colorize'
+require 'tty-prompt'
 array = []
 
 def workout
@@ -15,8 +16,29 @@ def workout
    "You did #{sets} sets of #{exercise} on " + time2.inspect
 end
 
+banner = """
+
+███████╗██╗██╗   ██╗███████╗    ██████╗ ██╗   ██╗    ███████╗██╗██╗   ██╗███████╗
+██╔════╝██║██║   ██║██╔════╝    ██╔══██╗╚██╗ ██╔╝    ██╔════╝██║██║   ██║██╔════╝
+█████╗  ██║██║   ██║█████╗      ██████╔╝ ╚████╔╝     █████╗  ██║██║   ██║█████╗  
+██╔══╝  ██║╚██╗ ██╔╝██╔══╝      ██╔══██╗  ╚██╔╝      ██╔══╝  ██║╚██╗ ██╔╝██╔══╝  
+██║     ██║ ╚████╔╝ ███████╗    ██████╔╝   ██║       ██║     ██║ ╚████╔╝ ███████╗
+╚═╝     ╚═╝  ╚═══╝  ╚══════╝    ╚═════╝    ╚═╝       ╚═╝     ╚═╝  ╚═══╝  ╚══════╝
+                                                                                 
+            ████████╗██████╗  █████╗ ██╗███╗   ██╗███████╗██████╗                
+            ╚══██╔══╝██╔══██╗██╔══██╗██║████╗  ██║██╔════╝██╔══██╗               
+               ██║   ██████╔╝███████║██║██╔██╗ ██║█████╗  ██████╔╝               
+               ██║   ██╔══██╗██╔══██║██║██║╚██╗██║██╔══╝  ██╔══██╗               
+               ██║   ██║  ██║██║  ██║██║██║ ╚████║███████╗██║  ██║               
+               ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝               
+                                                                                 
+
+"""
+
+puts banner.colorize(:yellow)
+puts'Welcome to the 5x5 Workout terminal application! Please select a number from 1-5'.colorize(:light_blue) 
 loop do
-  'Welcome to the 5x5 Workout terminal application! Please select a number from 1-5'.colorize(:light_blue)
+  
   puts "\n1) WHAT IS 5X5\n2) WEEK 1 EXERCISES\n3) WEEK 2 EXERCISES\n4) LOG A WORKOUT\n5) VIEW WORKOUT LOG\n6) EXIT".colorize(:blue)
   input = $stdin.gets.chomp.to_i
   case input
@@ -40,35 +62,4 @@ loop do
   end
 end
 
-# class FivebyFive
-# attr_accessor :workedout, :log
-# @workedout = false
-# @log = []
-
-#   def workedout
-#     $stdout.puts "Would you like to log a workout? y/n"
-#     inp = $stdin.gets.chomp
-#      if inp == n
-#       return
-#      elsif
-#      inp == y
-#      @workedout = true
-#      @log.<<("Workout logged!")
-#      else
-#   end
-
-# end
-# workout = 0
-# def workedout(a)  
-#   puts "have you worked out?"
-# input = gets.chomp
-#   if input == "yes" then a += 1
-#   puts "you have worked out #{a} times"
-#   return a
-#   else  
-# end
-# end
-# workedout(workout)
-
-# workedout(workout)
-
+# TTY Menu attempt
