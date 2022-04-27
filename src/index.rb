7661dc7c5f1225d207718a0421f113aedba3901f
VittorioBianco <vicbianco83@gmail.com>
2022-04-27 21:48:25 +1000

    added invalid entry prevention toworkout logger

--- src/index.rb
+++ src/index.rb
@@ -47,11 +47,22 @@ wolist = ['Squats', 'Overhead Press', 'Dead Lifts', 'Bench Press', 'Bent Over Ro
 
 def workout
   time = Time.new
-  $stdout.puts 'Which exercise would you like to log?'
+  $stdout.puts "Select from 'Squats', 'Overhead Press', 'Dead Lifts', 'Bench Press', 'Bent Over Row'".colorize(:light_blue)
+  $stdout.puts 'Which exercise would you like to log?'.colorize(:green)
   exercise = $stdin.gets.chomp
-  $stdout.puts 'How many sets did you perform?'
+  while(true)
+    break if (exercise == 'Squats' || exercise == 'Overhead Press' || exercise == 'Dead Lifts' ||exercise == 'Bench Press' || exercise == 'Bent Over Row')
+    puts "Invalid entry: #{exercise}.  Please enter a valid exercise!".colorize(:red)
+    exercise = $stdin.gets.chomp
+  end
+  $stdout.puts 'Using numbers, how many sets did you perform?'.colorize(:green)
   sets = $stdin.gets.chomp.to_i
-  "You did #{sets} sets of #{exercise} on " + time.strftime('%d/%m/%Y')
+  while(true)
+    break if (sets == 1 || sets == 2 || sets == 3 ||sets == 4 || sets == 5)
+    $stdout.puts "Invalid entry: #{sets}.  Please enter from 1 to 5.".colorize(:red)
+    sets = $stdin.gets.chomp.to_i
+  end
+  "You did #{sets} sets of #{exercise} on ".colorize(:light_blue) + time.strftime('%d/%m/%Y').colorize(:light_blue)
 end
 
 def clear
@@ -87,7 +98,7 @@ loop do
   when 4
     clear
     puts logger.colorize(:green)
-    $stdout.puts "The recommended 5x5 exercises are #{wolist.join(", ")}"
+    # $stdout.puts "The recommended 5x5 exercises are #{wolist.join(", ")}"
     array.push(workout)
   when 5
     clear
